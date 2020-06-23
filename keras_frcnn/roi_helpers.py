52da3eadacb01f4567172ffbd2a35616ca32f108
AnnJune <annjune622@yahoo.com>
2020-06-23 12:53:24 +0800

    commit

--- keras_frcnn/roi_helpers.py
+++ keras_frcnn/roi_helpers.py
@@ -38,7 +38,7 @@ def calc_iou(R, img_data, C, class_mapping):
         best_iou = 0.0
         best_bbox = -1
         for bbox_num in range(len(bboxes)):
-            print(bbox_num)
+            # print(bbox_num)
             curr_iou = data_generators.iou([gta[bbox_num, 0], gta[bbox_num, 2], gta[bbox_num, 1], gta[bbox_num, 3]], [x1, y1, x2, y2])
             if curr_iou > best_iou:
                 best_iou = curr_iou
