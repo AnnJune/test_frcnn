a1c950c61b38fcf533b4feace6a26b998f880565
AnnJune <annjune622@yahoo.com>
2020-06-26 09:14:17 +0800

    commit

--- train_frcnn.py
+++ train_frcnn.py
@@ -257,6 +257,8 @@ for epoch_num in range(num_epochs):
                 selected_pos_samples = pos_samples.tolist()
             else:
                 selected_pos_samples = np.random.choice(pos_samples, C.num_rois//2, replace=False).tolist()
+            if len(list(neg_samples))<1:
+                continue
             try:
                 selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
             except:
